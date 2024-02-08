64fa9cb6ed76da7b4999135df0b6cfe3e6582e9b
Sergey Pil <realsergeypil@gmail.com>
2024-02-08 14:48:21 +0600

    Spring foundation: Implement Spring Profiles

--- spring-foundation/src/test/java/net/serg/springfoundation/repository/UserRepositoryTest.java
+++ spring-foundation/src/test/java/net/serg/springfoundation/repository/UserRepositoryTest.java
@@ -3,11 +3,15 @@ package net.serg.springfoundation.repository;
 import net.serg.springfoundation.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class UserRepositoryTest {
 
     @Autowired
