17a3279895109fbc89b42d38d3cfedc117f53200
Sergey Pil <realsergeypil@gmail.com>
2024-02-08 14:21:27 +0600

    Spring foundation: Implement Conditional Configuration

--- spring-foundation/src/main/java/net/serg/springfoundation/config/DataSourceConfig.java
+++ spring-foundation/src/main/java/net/serg/springfoundation/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package net.serg.springfoundation.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -10,6 +11,7 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "custom.datasource.enabled", havingValue = "true", matchIfMissing = true)
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
